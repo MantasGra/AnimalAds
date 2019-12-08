829c0e340ef209150653a20b92d0400404bb7549
Mantas Grabauskas <mantelisas@gmail.com>
2019-12-08 22:42:45 +0200

    Add boost and report ad functionalities

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,9 +6,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ReportType;
+use App\Entity\Ad;
+use App\Entity\Boost;
+use App\Form\BoostType;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdController extends AbstractController
 {
+   
     /**
      * @Route(path="/ads", name="browse_ads")
      */
@@ -18,11 +24,14 @@ class AdController extends AbstractController
     }
 
     /**
-     * @Route(path="/ads/1", name="view_ad")
+     * @Route(path="/ads/{id}", name="view_ad")
      */
-    public function view()
+    public function view($id)
     {
-        return $this->render('ad/view.html.twig');
+        $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+        return $this->render('ad/view.html.twig', [
+            'ad' => $ad,
+        ]);
     }
 
     /**
@@ -34,18 +43,101 @@ class AdController extends AbstractController
     }
 
     /**
-     * @Route(path="/ads/report", name="report_ad")
+     * @Route(path="/ads/{id}/report", name="report_ad")
      */
-    public function report()
+    public function report($id, Request $request, \Swift_Mailer $mailer)
     {
-        return $this->render('ad/report.html.twig');
+        $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+        $form = $this->createForm(ReportType::class);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $this->getUser();
+            $emailMessage = (new \Swift_Message('An ad has been reported!'))
+            ->setFrom('bemantelio@gmail.com')
+            ->setTo('bemantelio@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    'ad/report-message.html.twig',
+                    ['ad' => $ad, 'user' => $user, 'message' => $form->getData()['reportMessage']]
+                ),
+                'text/html'
+            );
+        $mailer->send($emailMessage);
+            $this->addFlash('success', 'Thank you!');
+            return $this->redirectToRoute('view_ad', ['id' => $id]);
+        }
+        return $this->render('ad/report.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route(path="/ads/{id}/boost", name="boost_ad")
+     */
+    public function boost($id, Request $request)
+    {
+        $boost = new Boost();
+        $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+        $form = $this->createForm(BoostType::class, $boost);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $totalPrice = 0;
+            if ($boost->getType() == "Premium")
+            {
+                $totalPrice = 1;
+            }
+            else if ($boost->getType() == "Basic")
+            {
+                $totalPrice = 0.75;
+            }
+            switch($boost->getDuration())
+            {
+                case 3:
+                    $totalPrice *= 2.5;
+                    break;
+                case 7:
+                    $totalPrice *= 5.5;
+                    break;
+                case 30:
+                    $totalPrice *= 24;
+                    break;
+                default:
+                    break;
+            }
+
+            return $this->render('ad/pay.html.twig', [
+                'ad' => $ad,
+                'boost' => $boost,
+                'totalPrice' => $totalPrice
+            ]);
+        }
+
+        return $this->render('ad/boost.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView(),
+            'errors' => $form->getErrors(true)
+        ]);
     }
 
     /**
-     * @Route(path="/ads/boost", name="boost_ad")
+     * @Route(path="/ads/{id}/pay", name="pay_ad")
      */
-    public function boost()
+    public function pay($id, Request $request)
     {
-        return $this->render('ad/boost.html.twig');
+        $boost = new Boost();
+        $boost->setType($request->get('type'));
+        $boost->setDateFrom(new \DateTime($request->get('dateFrom')));
+        $boost->setDuration($request->get('duration'));
+        $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+        $boost->setAd($ad);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($boost);
+        $entityManager->flush();
+        $this->addFlash('success', 'Your add has been boosted. Thank you for choosing animal ads!');
+        return $this->redirectToRoute('view_ad', ['id' => $id]);
     }
 }
---
+++ src/Form/ReportType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ReportType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('reportMessage', TextareaType::class)
+        ;
+    }
+}
