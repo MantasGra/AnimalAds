1a220fbb401152117e2ff06f1b68059cbdff6b97
Karolis Samuolis <Tinshi@users.noreply.github.com>
2019-12-10 22:51:28 +0200

    comments/message
    Added functionality:
    Add comment,
    Edit comment,
    Delete comment,
    View comment,
    Reply to another comment,
    Send message to another user(missing file addition)

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -6,6 +6,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CommentType;
+use App\Form\MessageType;
+use App\Entity\Comment;
+use App\Entity\Message;
+use App\Entity\Ad;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class AdController extends AbstractController
 {
@@ -22,7 +30,194 @@ class AdController extends AbstractController
      */
     public function view()
     {
-        return $this->render('ad/view.html.twig');
+        $entityManager = $this->getDoctrine()->getManager();
+        $comments = $entityManager->getRepository('App:Comment')->findBy(
+            ['ad' => '1', 'parentComment' => null]
+        );
+        
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        return $this->render('ad/view.html.twig', [
+            'cmts' => $comments,
+            'form' => $form->createView(),
+            'error' => $form->getErrors(true)
+        ]);
+    }
+
+    /**
+     * @Route(path="/ads/{id}/{parent}/reply", name="replycomment")
+     */
+    public function reply(Request $request, $id, $parent)
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        return $this->render('ad/reply.html.twig', [
+            'id' => $id,
+            'parent' => $parent,
+            //'parentComment' => $parentComment,
+            'form' => $form->createView(),
+            'error' => $form->getErrors(true)
+        ]);
+    }
+
+    /**
+     * @Route(path="/ads/{id}/{parent}/reply/success", name="submitreply")
+     */
+    public function replysubmit(Request $request, $id, $parent)
+    {
+        
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+            $parentComment = $this->getDoctrine()->getRepository(Comment::class)->find($parent);
+            $comment -> setAd($ad);
+            $comment -> setWrittenBy($this->getUser());
+            $comment -> setCreatedAt(new \DateTime());
+            $comment -> setParentComment($parentComment);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            $this->addFlash('success', 'Your reply was added');
+            return $this->redirectToRoute('view_ad');
+        }
+        $this->addFlash('warning', 'Something went wrong');
+        return $this->redirectToRoute('view_ad');
+    }
+
+    /**
+     * @Route(path="/ads/{id}/contact", name="contact")
+     */
+    public function contact(Request $request, $id)
+    {
+
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        return $this->render('ad/contact.html.twig',[
+            'id' => $id,
+            'form' => $form->createView(),
+            'error' => $form->getErrors(true)
+        ]);
+    }
+
+    /**
+     * @Route(path="/ads/{id}/contact/success", name="submitcontact")
+     */
+    public function contactsubmit(Request $request, $id)
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+            $message -> setSentTo($ad->getCreatedBy());
+            $message -> setSentFrom($this->getUser());
+            $message -> setWrittenAt(new \DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($message);
+            $entityManager->flush();
+            $this->addFlash('success', 'Your message was sent');
+            return $this->redirectToRoute('view_ad');
+
+            $emailMessage = (new \Swift_Message('Message to seller'))
+                ->setFrom('bemantelio@gmail.com')
+                ->setTo($ad->getCreatedBy()->getEmail())
+                ->setBody(
+                    $this->renderView(
+                        'message/email-message.html.twig'
+                    ),
+                    'text/html'
+                );
+            $mailer->send($emailMessage);
+        }
+        $this->addFlash('warning', 'Something went wrong');
+        return $this->redirectToRoute('view_ad');
+    }
+
+    /**
+     * @Route(path="/ads/{id}/{commid}/edit", name="editcomment")
+     */
+    public function editcomment(Request $request, $id, $commid)
+    {
+        $comment = $this->getDoctrine()->getRepository(Comment::class)->find($commid);
+        $form = $this->createForm(CommentType::class, $comment);
+        return $this->render('ad/edit-comment.html.twig', [
+            'id' => $id,
+            'commid' => $commid,
+            'form' => $form->createView(),
+            'error' => $form->getErrors(true)
+        ]);
+    }
+
+    /**
+     * @Route(path="/ads/{id}/{commid}/edit/success", name="submitedit")
+     */
+    public function editcommentsubmit(Request $request, $id, $commid)
+    {
+        //$user = $this->getUser();
+        $comment = $this->getDoctrine()->getRepository(Comment::class)->find($commid);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($comment->getWrittenBy()->getId() == $this->getUser()->getId()) {
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($comment);
+                $entityManager->flush();
+                $this->addFlash('success', 'Your comment was changed');
+                return $this->redirectToRoute('view_ad');
+            }
+            $this->addFlash('warning', 'Something went wrong');
+            return $this->redirectToRoute('view_ad');
+        }
+        $this->addFlash('warning', 'You are not the author of the comment.');
+        return $this->redirectToRoute('view_ad');
+    }
+
+
+
+    /**
+     * @Route(path="/ads/{id}/comment/", name="comment")
+     */
+    public function comment(Request $request, $id){
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ad = $this->getDoctrine()->getRepository(Ad::class)->find($id);
+            $comment -> setAd($ad);
+            $comment -> setWrittenBy($this->getUser());
+            $comment -> setCreatedAt(new \DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            $this->addFlash('success', 'Your comment was added');
+            return $this->redirectToRoute('view_ad');
+        }
+        $this->addFlash('warning', 'Something went wrong');
+        return $this->redirectToRoute('view_ad');
+    }
+
+    /**
+     * @Route(path="/ads/{adid}/{id}/deletecomment/", name="deletecomment")
+     */
+    public function deleteComment(Request $request, $id){
+        $user = $this->getUser();
+        $comment = $this->getDoctrine()->getRepository(Comment::class)->find($id);
+        if ($comment->getWrittenBy()->getId() == $user->getId()) {
+            if (count($comment->getReplies()) == 0) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->remove($comment);
+                $entityManager->flush();
+                $this->addFlash('success', 'Your comment was deleted');
+                return $this->redirectToRoute('view_ad');
+            }
+            $this->addFlash('warning', 'This comment has replies.');
+            return $this->redirectToRoute('view_ad');
+        }
+        $this->addFlash('warning', 'You are not the author of the comment.');
+        return $this->redirectToRoute('view_ad');
     }
 
     /**
---
+++ src/Form/CommentType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Comment...'
+                ]
+            ])
+        ;
+    }
+    public function getText(){
+        return $this->text;
+    }
+
+    public function setText($text){
+        $this->text = $text;
+    }
+    public function __constructor($formname)
+    {
+        $this->text = $formname;
+        parent::__construct();
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
